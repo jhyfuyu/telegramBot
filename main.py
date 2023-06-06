a9b6de38a168681e210bd0a86328781ec4822745
Пк Dell <rusibr29@gmail.com>
2023-06-07 01:22:19 +0400

    добавлена отправка сообщений. бот не активен, предстоит написать обработку изменений

--- main.py
+++ main.py
@@ -2,22 +2,32 @@
 import requests
 from config import *
 from bs4 import BeautifulSoup
-import telebot as tb
+import telebot
+import time
+
+orders_list = None
 
 def order_parsing(x):
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.text,'html.parser')
-    orders_list = soup.find('div',class_="task__title") #парсим основную инфу
-    print(orders_list.text,end='. ')
-    return soup
+    while True:
+        global orders_list
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.text,'html.parser')
+        orders_list = (soup.find('div',class_="task__title")).text #парсим основную инфу
+        return soup
+        time.sleep(1800)
 
-def order_info_parsing(resp,info):
+def order_info_parsing(resp,flag):
     order_data = (resp.find('span',class_="count")).text
-    print(order_data)
+    return order_data
 
 resp = order_parsing(None)
 if __name__ == '__main__':
-    # parsing(None)
-    def data_treatment():  # отправка данных в самого бота
-        pass
+    bot=telebot.TeleBot(TOKEN)
+    @bot.message_handler(commands=['start'])
 
+    def data_treatment(message):  # отправка данных в самого бота
+        while True:
+            bot.send_message(message.from_user.id,orders_list)
+            bot.send_message(message.from_user.id, order_info_parsing(resp,False))
+            time.sleep(1800)
+    bot.infinity_polling()
