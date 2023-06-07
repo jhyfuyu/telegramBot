ddde8720ecd48a330ee32927adc05ff43bab3dc3
Пк Dell <rusibr29@gmail.com>
2023-06-07 13:59:45 +0400

    some changes for sending data to Telegram

--- main.py
+++ main.py
@@ -6,28 +6,34 @@ import telebot
 import time
 
 orders_list = None
+def order_parsing():
+    old_value = None
 
-def order_parsing(x):
+    global orders_list
+
     while True:
-        global orders_list
         response = requests.get(URL)
         soup = BeautifulSoup(response.text,'html.parser')
         orders_list = (soup.find('div',class_="task__title")).text #парсим основную инфу
-        return soup
+        list1 = [soup,orders_list]
+        return list1
         time.sleep(1800)
 
-def order_info_parsing(resp,flag):
-    order_data = (resp.find('span',class_="count")).text
+
+def order_info_parsing(flag):
+    initialDataForParsing = order_parsing()
+    order_data = (initialDataForParsing[0].find('span',class_="count")).text
     return order_data
 
-resp = order_parsing(None)
+
 if __name__ == '__main__':
     bot=telebot.TeleBot(TOKEN)
     @bot.message_handler(commands=['start'])
 
     def data_treatment(message):  # отправка данных в самого бота
         while True:
-            bot.send_message(message.from_user.id,orders_list)
-            bot.send_message(message.from_user.id, order_info_parsing(resp,False))
+            order_name = order_parsing()
+            bot.send_message(message.from_user.id,order_name[1])
+            bot.send_message(message.from_user.id, order_info_parsing(False))
             time.sleep(1800)
     bot.infinity_polling()
